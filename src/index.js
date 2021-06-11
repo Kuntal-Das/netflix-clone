c662a5e249ec73cd4ccf31a00bf9c76484c11b26
Kuntal Das <25ckuntaldas@gmail.com>
2021-06-11 07:43:18 +0000

    firebase setup

--- src/index.js
+++ src/index.js
@@ -4,11 +4,29 @@ import reportWebVitals from "./reportWebVitals";
 
 import App from "./App";
 import { GlobalStyles } from "./GlobalStyles";
+import { FirebaseContext } from "./context/firebase";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const config = {
+  apiKey: "AIzaSyCleiwTPkpprInQo0jrUdLqkrRcBzplsgs",
+  authDomain: "animesta-bd102.firebaseapp.com",
+  projectId: "animesta-bd102",
+  storageBucket: "animesta-bd102.appspot.com",
+  messagingSenderId: "129360534327",
+  appId: "1:129360534327:web:71266b4d7dabfa895a18b2",
+  measurementId: "G-MCEXZ5C34X"
+}
+// Initialize Firebase
+const firebase = window.firebase.initializeApp(config);
+firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
