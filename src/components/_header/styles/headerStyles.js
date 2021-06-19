b80a10bf4983c8439a28bfb4f9cfecff28424c97
Kuntal Das <25ckuntaldas@gmail.com>
2021-06-19 21:21:00 +0530

    renamed comp NewHeader -> Header, renamed folder header -> _header

---
+++ src/components/_header/styles/headerStyles.js
@@ -0,0 +1,221 @@
+import styled from "styled-components";
+import { Link as ReachRouterLink } from "react-router-dom";
+
+export const Background = styled.section`
+  display: flex;
+  flex-direction: column;
+  background: url(${({ src }) => (src ? src : "../images/misc/home-bg.jpg")})
+    top left / cover no-repeat;
+
+  @media (max-width: 1100px) {
+    ${({ dontShowOnSmallViewPort }) =>
+    dontShowOnSmallViewPort && "background:none"}
+  }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  margin: 0 3.5em;
+  padding: 1.125em 0;
+  justify-content: space-between;
+  align-items: center;
+
+  a {
+    display: flex;
+  }
+
+  @media (max-width: 1000px) {
+    margin: 0 1.875em;
+  }
+`;
+
+export const Link = styled.p`
+  color: #fff;
+  text-decoration: none;
+  margin-right: 30px;
+  font-weight: ${({ active }) => (active === "true" ? "700" : "normal")};
+  cursor: pointer;
+
+  &:hover {
+    font-weight: bold;
+  }
+  &:last-of-type {
+    margin-right: 0;
+  }
+`;
+
+export const ButtonLink = styled(ReachRouterLink)`
+  display: block;
+  background-color: #e50914;
+  height: fit-content;
+  color: white;
+  border: 0;
+  font-size: 0.9rem;
+  border-radius: 3px;
+  padding: 0.5em 1em;
+  cursor: pointer;
+  text-decoration: none;
+  text-transform: capitalize;
+  box-sizing: border-box;
+
+  &:hover {
+    background: #f40612;
+  }
+`;
+
+export const Logo = styled.img`
+  height: 32px;
+  width: 108px;
+  margin-right: 2.5em;
+
+  @media (min-width: 1449px) {
+    height: 45px;
+    width: 167px;
+  }
+`;
+
+export const Group = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Text = styled.p`
+  color: white;
+  font-size: 1.375rem;
+  line-height: 1.5;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
+`;
+
+export const Feature = styled(Container)`
+  padding: 9em 0 31.25em 0;
+  flex-direction: column;
+  align-items: normal;
+  width: 50%;
+
+  @media (max-width: 1100px) {
+    display: none;
+  }
+`;
+
+export const FeatureCallOut = styled.h2`
+  color: white;
+  font-size: 3.125rem;
+  line-height: 1.5;
+  font-weight: 700;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
+  margin: 0;
+`;
+
+export const PlayButton = styled.button`
+  box-shadow: 0 0.6vw 1vw -0.4vw rgba(0, 0, 0, 0.35);
+  background-color: #e6e6e6;
+  color: #000;
+  border-width: 0;
+  padding: 0.5em 1em;
+  border-radius: 5px;
+  max-width: 130px;
+  font-weight: bold;
+  font-size: 1.25rem;
+  margin-top: 0.5em;
+  cursor: pointer;
+  transition: background-color 0.5s ease, color 0.5s ease;
+
+  &:hover {
+    background: #ff1e1e;
+    color: white;
+  }
+`;
+
+export const Search = styled.div`
+  display: flex;
+  align-items: center;
+
+  @media (max-width: 700px) {
+    display: none;
+  }
+`;
+
+export const SearchIcon = styled.button`
+  cursor: pointer;
+  background-color: transparent;
+  border: 0;
+
+  img {
+    filter: brightness(0) invert(1);
+    width: 16px;
+  }
+`;
+
+export const SearchInput = styled.input`
+  background-color: #44444459;
+  color: white;
+  border: 1px solid white;
+  transition: width 0.5s;
+  height: 30px;
+  font-size: 0.875rem;
+  margin-left: ${({ active }) => (active === true ? "0.75em" : "0")};
+  padding: ${({ active }) => (active === true ? "0 0.75em" : "0")};
+  opacity: ${({ active }) => (active === true ? "1" : "0")};
+  width: ${({ active }) => (active === true ? "200px" : "0px")};
+`;
+
+export const Picture = styled.button`
+  background: url(${({ src }) => src});
+  background-size: contain;
+  border: 0;
+  width: 32px;
+  height: 32px;
+  cursor: pointer;
+`;
+
+export const Dropdown = styled.div`
+  display: none;
+  position: absolute;
+  background-color: black;
+  padding: 0.625em;
+  width: 100px;
+  top: 32px;
+  right: 10px;
+
+  ${Group}:last-of-type ${Link} {
+    cursor: pointer;
+  }
+
+  ${Group} {
+    margin-bottom: 0.625em;
+
+    &:last-of-type {
+      margin-bottom: 0;
+    }
+
+    ${Link}, ${Picture} {
+      cursor: default;
+    }
+  }
+
+  button {
+    margin-right: 0.625em;
+  }
+
+  p {
+    font-size: 0.75rem;
+    margin-bottom: 0;
+    margin-top: 0;
+  }
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 1.25em;
+  position: relative;
+
+  button {
+    cursor: pointer;
+  }
+
+  &:hover > ${Dropdown} {
+    display: flex;
+    flex-direction: column;
+  }
+`;
