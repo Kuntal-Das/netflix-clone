baa2f8ccc21fa5365417f6b40b8c6de33f61c0fe
Kuntal Das <25ckuntaldas@gmail.com>
2021-06-19 21:21:00 +0530

    renamed comp NewHeader -> Header, renamed folder header -> _header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,102 +1,64 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import { Container, Group, Logo, StyledHeader, Link, Search, SearchIcon, SearchInput } from "./styles/newHeaderStyles";
 
-import { Link as ReachRouterLink } from "react-router-dom";
-import {
-  Background,
-  ButtonLink,
-  Container,
-  Group,
-  Logo,
-  Link,
-  Feature,
-  FeatureCallOut,
-  PlayButton,
-  Text,
-  Search,
-  SearchIcon,
-  SearchInput,
-  Picture,
-  Profile,
-  Dropdown
-} from "./styles/headerStyles";
+export default function Header({ children, restProps }) {
 
-export default function Header({ isBgvisible = true, children, ...restProps }) {
-  return isBgvisible ? (
-    <Background {...restProps}>{children}</Background>
-  ) : (
-    children
-  );
+    return (
+        <StyledHeader {...restProps}>{children}</StyledHeader>
+    )
 }
 
-Header.Logo = ({ to, ...restProps }) => (
-  <ReachRouterLink to={to}>
-    <Logo {...restProps} />
-  </ReachRouterLink>
-);
-
-Header.Frame = ({ children, ...restProps }) => (
-  <Container {...restProps}>{children}</Container>
-);
+Header.Container = ({children,...restProps}) => (
+    <Container {...restProps}>{ children }</Container>
+)
 
 Header.Group = ({ children, ...restProps }) => (
-  <Group {...restProps}>{children}</Group>
-);
+    <Group {...restProps}>{children}</Group>
+)
 
-Header.Text = ({ children, ...restProps }) => (
-  <Text {...restProps}>{children}</Text>
+Header.Logo = ({ to, ...restProps }) => (
+    <Link to={to} noStyle={true} >
+        <Logo {...restProps} />
+    </Link>
 );
 
 Header.Link = ({ children, ...restProps }) => (
-  <Link {...restProps}>{children}</Link>
+    <Link {...restProps}>{children}</Link>
 );
 
-Header.ButtonLink = ({ children, ...restProps }) => (
-  <ButtonLink {...restProps}>{children}</ButtonLink>
-);
-
-Header.Feature = ({ children, ...restProps }) => (
-  <Feature {...restProps}>{children}</Feature>
-);
-
-Header.FeatureCallOut = ({ children, ...restProps }) => (
-  <FeatureCallOut {...restProps}>{children}</FeatureCallOut>
-);
-
-Header.PlayButton = ({ children, ...restProps }) => (
-  <PlayButton {...restProps}>{children}</PlayButton>
-);
-
-Header.Search = function HeaderSearch({
-  searchTerm,
-  setSearchTerm,
-  ...restProps
+Header.Search = function NewHeaderSearch({
+    searchTerm,
+    setSearchTerm,
+    slides,
+    ...restProps
 }) {
-  const [searchActive, setSearchActive] = useState(false);
-  const toggleSearchBar = () => {
-    if (searchTerm) setSearchTerm("");
-    setSearchActive(!searchActive)
-  }
-  return (
-    <Search {...restProps}>
-      <SearchIcon onClick={toggleSearchBar}>
-        <img src="/images/icons/search.png" alt="Search" />
-      </SearchIcon>
-      <SearchInput
-        value={searchTerm}
-        onChange={({ target }) => setSearchTerm(target.value)}
-        placeholder="Search files and series"
-        active={searchActive}
-      />
-    </Search>
-  );
-};
+    // const [searchTerm, setSearchTerm] = useState("");
+    const searchInputRef = useRef(null);
+    const [searchActive, setSearchActive] = useState(false);
 
-Header.Picture = ({ src, ...restProps }) => (
-  <Picture {...restProps} src={src} />
-);
-Header.Profile = ({ children, ...restProps }) => (
-  <Profile {...restProps}>{children}</Profile>
-);
-Header.Dropdown = ({ children, ...restProps }) => (
-  <Dropdown {...restProps}>{children}</Dropdown>
-);
+    const toggleSearchBar = () => {
+        if (searchTerm) setSearchTerm("");
+        setSearchActive(!searchActive)
+        if(searchInputRef && !searchActive) searchInputRef.current.focus();
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(`TODO: searchbar : handleSubmit`)
+    }
+
+    return (
+        <Search {...restProps} onSubmit={ handleSubmit }>
+            <SearchIcon onClick={toggleSearchBar} type="button">
+                <img src="/images/icons/search.png" alt="Search" />
+            </SearchIcon>
+            <SearchInput
+                ref={searchInputRef}
+                value={searchTerm}
+                onChange={({ target }) => setSearchTerm(target.value)}
+                placeholder="Search files and series"
+                active={searchActive}
+            />
+        </Search>
+    );
+}
