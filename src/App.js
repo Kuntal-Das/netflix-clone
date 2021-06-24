0947289bf669d7c926e0d4c890d56e96a78918f7
Kuntal Das <25ckuntaldas@gmail.com>
2021-06-24 08:58:58 +0530

    removed dependency : use-auth-listner

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
-
 import { Browse, Home, SignIn, SignUp } from "./pages";
 import * as ROUTES from "./constants/routes";
-import { useAuthListener } from "./hooks";
 import { RouteWithRedirect, ProtectedRoute } from "./utils/routes";
+import { FirebaseContext } from "./context/firebase";
 
-function App() {
-  const { user } = useAuthListener();
+class App extends Component {
+  state = { user: null }
+  static contextType = FirebaseContext
 
-  return (
+  componentDidMount = () => {
+    const { firebase } = this.context
+    this.unsubscribe = firebase.auth().onAuthStateChanged(currUser=> {
+      if(currUser){
+        this.setState({ user : currUser })
+      }else{
+        this.setState({ user: null });
+      }
+    })
+  }
+
+  componentWillUnmount = () => this.unsubscribe()
+
+  render = () => (
     <Router>
       <Switch>
         <RouteWithRedirect
           exact
           path={ROUTES.HOME}
-          user={user}
+          user={this.state.user}
           loggedInPath={ROUTES.BROWSE}
         >
           <Home />
@@ -23,7 +36,7 @@ function App() {
 
         <RouteWithRedirect
           path={ROUTES.SIGN_IN}
-          user={user}
+          user={this.state.user}
           loggedInPath={ROUTES.BROWSE}
         >
           <SignIn />
@@ -31,18 +44,56 @@ function App() {
 
         <RouteWithRedirect
           path={ROUTES.SIGN_UP}
-          user={user}
+          user={this.state.user}
           loggedInPath={ROUTES.BROWSE}
         >
           <SignUp />
         </RouteWithRedirect>
 
-        <ProtectedRoute path={ROUTES.BROWSE} user={user}>
-          <Browse user={user}/>
+        <ProtectedRoute path={ROUTES.BROWSE} user={this.state.user}>
+          <Browse user={this.state.user}/>
         </ProtectedRoute>
       </Switch>
     </Router>
   );
 }
+// function App() {
+//   const { user } = useAuthListener();
+
+//   return (
+//     <Router>
+//       <Switch>
+//         <RouteWithRedirect
+//           exact
+//           path={ROUTES.HOME}
+//           user={user}
+//           loggedInPath={ROUTES.BROWSE}
+//         >
+//           <Home />
+//         </RouteWithRedirect>
+
+//         <RouteWithRedirect
+//           path={ROUTES.SIGN_IN}
+//           user={user}
+//           loggedInPath={ROUTES.BROWSE}
+//         >
+//           <SignIn />
+//         </RouteWithRedirect>
+
+//         <RouteWithRedirect
+//           path={ROUTES.SIGN_UP}
+//           user={user}
+//           loggedInPath={ROUTES.BROWSE}
+//         >
+//           <SignUp />
+//         </RouteWithRedirect>
+
+//         <ProtectedRoute path={ROUTES.BROWSE} user={user}>
+//           <Browse user={user}/>
+//         </ProtectedRoute>
+//       </Switch>
+//     </Router>
+//   );
+// }
 
 export default App;
